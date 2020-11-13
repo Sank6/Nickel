e68cbe2a1f6d092fb12198f7f997bdc802581a96
Sank6 <25014241+Sank6@users.noreply.github.com>
2020-11-13 16:29:31 +0000

    Change colours on bigrams

--- src/ui/assets/js/ngrams.js
+++ src/ui/assets/js/ngrams.js
@@ -4,7 +4,7 @@ $(document).ready(() => {
         let cipher = $("#cipher-selector").text().toLowerCase()
         let bigrams = ngrams(2, $(`#${cipher}-body`).val())
         let bigram_map = bigrams.map(x => 
-            `<span style="color: rgb(${(255 * x[1]/bigrams[0][1])}, ${300-(255 * x[1]/bigrams[0][1])}, 20)">${x[0]}: ${x[1]}</span>`
+            `<span style="color: rgb(${255-(255 * x[1]/bigrams[0][1])}, ${(255 * x[1]/bigrams[0][1])}, 20)">${x[0]}: ${x[1]}</span>`
             ).join("<br />")
         $(".bigrams").html(bigram_map)
     }
