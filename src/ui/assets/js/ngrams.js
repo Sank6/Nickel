944e88257eb9f0f52de27a45b85234fdabd5916f
Sank6 <25014241+Sank6@users.noreply.github.com>
2020-11-13 17:13:49 +0000

    Add trigrams and quadgrams to popup

--- src/ui/assets/js/ngrams.js
+++ src/ui/assets/js/ngrams.js
@@ -2,11 +2,13 @@ const ngrams = require("./assets/js/tools/ngrams.js");
 $(document).ready(() => {
     function update() {
         let cipher = $("#cipher-selector").text().toLowerCase()
-        let bigrams = ngrams(2, $(`#${cipher}-body`).val())
-        let bigram_map = bigrams.map(x => 
-            `<span style="color: rgb(${255-(255 * x[1]/bigrams[0][1])}, ${(255 * x[1]/bigrams[0][1])}, 20)">${x[0]}: ${x[1]}</span>`
-            ).join("<br />")
-        $(".bigrams").html(bigram_map)
+        for (n = 2; n <= 4; n ++) {
+            let ng = ngrams(n, $(`#${cipher}-body`).val())
+            let ng_map = ng.slice(0, 30).map(x => 
+                `<span style="color: rgb(${255-(255 * x[1]/ng[0][1])}, ${(255 * x[1]/ng[0][1])}, 20)">${x[0]}: ${x[1]}</span>`
+                ).join("<br />")
+            $(`.${["", "", "bigrams", "trigrams", "quadgrams"][n]}`).html(ng_map)
+        }
     }
 
     update()
--- src/ui/assets/js/utils/clean.js
+++ src/ui/assets/js/utils/clean.js
@@ -1,3 +1,3 @@
 module.exports = function(string) {
-    return string.toUpperCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()'"]/g,"").replace(/\s/g,"");
+    return string.toUpperCase().replace(/[^A-Za-z]/g,"").replace(/\s/g,"");
 }
